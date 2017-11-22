093a4a49cf549426ddf9b2b534e60a5d9a85f53a
Tim Paine <t.paine154@gmail.com>
2017-11-22 16:33:59 -0500

    style updates

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-#!flask/bin/python
 import os
 from webfram import app
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True, port=os.environ.get('PORT', 8080))
--- webfram/__init__.py
+++ webfram/__init__.py
@@ -13,9 +13,13 @@ def index():
     blocks = []
     blocks.append({'title': 'Find Places of Worship in Your Area',
                    'span1': 'Local Church is your reference for places of worship in your local community.',
-                   'searchtext':'Search by religious affiliation, state, county, city, or zipcode'})
-    blocks.append({'title': 'Section 2', 'span1':'Span 2', 'subtitle': 'Subtitle2', 'span2': 'Span 2 - 2'})
-    blocks.append({'title': 'Section 3', 'span1':'Span 3', 'subtitle': 'Subtitle3', 'span2': 'Span 3 - 2'})
+                   'searchtext': 'Search by affiliation, state, city, or zipcode',
+                   'class': 'darkbox'})
+    blocks.append({'title': 'Section 2', 'span1': 'Span 2', 'subtitle': 'Subtitle2', 'span2': 'Span 2 - 2', 'class': 'lightbox'})
+    blocks.append({'title': 'Section 3', 'span1': 'Span 3', 'subtitle': 'Subtitle3', 'span2': 'Span 3 - 2'})
+    blocks.append({'title': 'Section 4', 'span1': 'Span 4', 'subtitle': 'Subtitle4', 'span2': 'Span 4 - 2'})
+    blocks.append({'title': 'Section 5', 'span1': 'Span 5', 'subtitle': 'Subtitle5', 'span2': 'Span 5 - 2', 'class': 'clearbox'})
+    blocks.append({'title': 'Section 6', 'span1': 'Span 6', 'subtitle': 'Subtitle6', 'span2': 'Span 6 - 2', 'class': 'clearbox'})
 
     render['blocks'] = blocks
     render['title'] = 'Local Church'
