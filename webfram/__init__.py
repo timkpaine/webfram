883f6fdc74f5e6312618a10c8b552d9bf46a8887
Tim Paine <t.paine154@gmail.com>
2017-12-15 04:52:00 +0000

    working on state stuff

--- webfram/__init__.py
+++ webfram/__init__.py
@@ -2,17 +2,19 @@ import configparser
 import os
 import ujson
 from flask import Flask, render_template, redirect
-
+from .loader import read_state
 
 app = Flask(__name__, static_url_path='/static')
 
 
 def main():
     sites = {}
+    states = {}
     for site in os.listdir('sites'):
         c = configparser.ConfigParser()
         c.read('./sites/%s/%s.cfg' % (site, site))
         sites[site] = c
+        states[site] = read_state('./sites/%s/states.csv' % site)
 
     @app.route('/<site>')
     @app.route('/<site>/')
@@ -34,10 +36,12 @@ def main():
     @app.route('/<site>/<state>/')
     def site_states(site='church', state=None):
         c = sites[site]
+        vals = states[site].get(state, [])
         render = {}
         render['site'] = site
         render['title'] = c.get('main', 'title')
         render['favicon'] = c.get('main', 'favicon')
+        render['vals'] = vals
         return render_template("state.html", **render)
 
     @app.route('/index')
